e431554f6078217968b85faba2f69754dd4cc2fa
Jeff-Oliveira <JeffersonH.Feernandes@gmail.com>
2022-09-12 10:21:44 -0300

    att - main.h
    Foram movidos os vetores de interrupcao para o arquivo de cabecalho para melhor arrumacao do codigo

--- include/main.h
+++ include/main.h
@@ -13,9 +13,49 @@
 #include "sinalizacao_visual.h"
 
 #define set_bit(y,bit)      (y|=(1<<bit)) //coloca em 1 o bit x da variável Y 
-#define clear_bit(y,bit)    (y&=~(1<<bit)) //coloca em 0 o bit x da variável Y 
+#define clr_bit(y,bit)    (y&=~(1<<bit)) //coloca em 0 o bit x da variável Y 
 #define cpl_bit(y,bit)      (y^=(1<<bit)) //troca o estado lógico do bit x da variável Y 
 #define tst_bit(y,bit)      (y&(1<<bit)) //retorna 0 ou 1 conforme leitura do bit
+
+uint8_t ch;
+uint8_t buffer[];
+
+ISR(TIMER0_OVF_vect) 
+{
+    TCNT0 = 252; // recarrega o Timer 0 para contagem de 250us
+    f_timers();
+}
+
+ISR(PCINT0_vect)
+{
+    if(!tst_bit(PINB, BOTAO_0))
+    {
+        //cpl_bit(PORTB, LED_ON_OFF);
+    }
+    else if(!tst_bit(PINB, BOTAO_1))
+    {
+        cpl_bit(PORTD, LED_SOM);   
+    }
+    else if(!tst_bit(PINB, BOTAO_2))
+    {
+        cpl_bit(PORTD, LED_VISUAL);   
+    }
+    else if(!tst_bit(PINB, BOTAO_3))
+    {
+
+    }
+}
+
+ISR(USART_RX_vect) 
+{
+    ch = UDR0;    // Faz a leitura do buffer da serial 
+    UDR0 = ch;    // envia pela serial o valor lido  
+}
+
+ISR(ADC_vect)
+{
+    read_ad_chanel();
+}
 
 void setup();
 void setup_software();
