d81a5db6be4810d33fecc418ec07ffe416933122
Jeff-Oliveira <JeffersonH.Feernandes@gmail.com>
2022-09-20 08:56:33 -0300

    created - project menu

---
+++ include/menu_control.h
@@ -0,0 +1,5 @@
+void select_function(uint8_t menu_flag);
+void exit_menu(void);
+void init_menu(void);
+void function_visual(void);
+void function_alarm(void);
---
+++ src/menu_control.c
@@ -0,0 +1,61 @@
+#include "hardware_config.h"
+#include "contador.h"
+#include "sinalizacao_visual.h"
+#include "menu_control.h"
+
+#define set_bit(y,bit)      (y|=(1<<bit)) //coloca em 1 o bit x da variável Y 
+#define clr_bit(y,bit)      (y&=~(1<<bit)) //coloca em 0 o bit x da variável Y 
+#define cpl_bit(y,bit)      (y^=(1<<bit)) //troca o estado lógico do bit x da variável Y 
+#define tst_bit(y,bit)      (y&(1<<bit)) //retorna 0 ou 1 conforme leitura do bit
+
+// int8_t menu_flag = 0, confirm_flag = 0;
+
+void select_function(uint8_t menu_flag)
+{
+    static uint8_t option;
+    
+    option = menu_flag;
+
+    switch(menu_flag)
+    {
+        case 0:
+            exit_menu();
+            break;
+        case 1:
+            init_menu();
+            break;
+        case 2:
+            function_visual();
+            break;
+        case 3:
+            function_alarm();
+            break;
+        // case 4:
+        //     function_alarm();
+        //     break;    
+    }
+}
+
+void init_menu()
+{
+    set_bit(PORTD, LED_VISUAL);
+    set_bit(PORTD, LED_ALARM);
+}
+
+void exit_menu()
+{
+    clr_bit(PORTD, LED_VISUAL);
+    clr_bit(PORTD, LED_ALARM);
+}
+
+void function_visual()
+{
+    cpl_bit(PORTD, LED_VISUAL);
+    set_bit(PORTD, LED_ALARM);
+}
+
+void function_alarm()
+{
+    set_bit(PORTD, LED_VISUAL);
+    cpl_bit(PORTD, LED_ALARM);
+}
